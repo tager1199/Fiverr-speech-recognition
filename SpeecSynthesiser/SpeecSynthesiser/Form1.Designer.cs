92a0789621a2729cee470ed505ca45031f8796b7
tager1199 <32684792+tager1199@users.noreply.github.com>
2020-09-05 14:02:02 +0100

    Speech Regonition
    Added speech regonition, uses windows built in regonition, not 100% accurate

--- SpeecSynthesiser/SpeecSynthesiser/Form1.Designer.cs
+++ SpeecSynthesiser/SpeecSynthesiser/Form1.Designer.cs
@@ -28,34 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.runBtn = new System.Windows.Forms.Button();
+            this.SpeechBox = new System.Windows.Forms.RichTextBox();
+            this.Copy_Btn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // runBtn
+            // SpeechBox
             // 
-            this.runBtn.Location = new System.Drawing.Point(311, 224);
-            this.runBtn.Name = "runBtn";
-            this.runBtn.Size = new System.Drawing.Size(75, 23);
-            this.runBtn.TabIndex = 0;
-            this.runBtn.Text = "Run";
-            this.runBtn.UseVisualStyleBackColor = true;
-            this.runBtn.Click += new System.EventHandler(this.runBtn_Click);
+            this.SpeechBox.Location = new System.Drawing.Point(13, 13);
+            this.SpeechBox.Name = "SpeechBox";
+            this.SpeechBox.Size = new System.Drawing.Size(504, 396);
+            this.SpeechBox.TabIndex = 1;
+            this.SpeechBox.Text = "";
+            // 
+            // Copy_Btn
+            // 
+            this.Copy_Btn.Location = new System.Drawing.Point(93, 415);
+            this.Copy_Btn.Name = "Copy_Btn";
+            this.Copy_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Copy_Btn.TabIndex = 2;
+            this.Copy_Btn.Text = "Copy";
+            this.Copy_Btn.UseVisualStyleBackColor = true;
+            this.Copy_Btn.Click += new System.EventHandler(this.Copy_Btn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.runBtn);
+            this.ClientSize = new System.Drawing.Size(529, 450);
+            this.Controls.Add(this.Copy_Btn);
+            this.Controls.Add(this.SpeechBox);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button runBtn;
+        private System.Windows.Forms.RichTextBox SpeechBox;
+        private System.Windows.Forms.Button Copy_Btn;
     }
 }
 
--- SpeecSynthesiser/SpeecSynthesiser/Form1.cs
+++ SpeecSynthesiser/SpeecSynthesiser/Form1.cs
@@ -1,44 +1,57 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Speech.Recognition;
+using System.IO;
 
 namespace SpeecSynthesiser
 {
+
     public partial class Form1 : Form
     {
-        SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine();
+        SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-GB"));
         SpeechRecognitionEngine startListening = new SpeechRecognitionEngine();
-        Random rnd = new Random();
-        int RecTimeOut = 0;
 
         public Form1()
         {
-            
             InitializeComponent();
         }
 
-        private void runBtn_Click(object sender, EventArgs e)
+        public static void ChangeText(String message)
         {
-            using (SpeechRecognitionEngine recognizer =
-                new SpeechRecognitionEngine(
-                    new System.Globalization.CultureInfo("en-GB")))
-            {
-                recognizer.LoadGrammar(new DictationGrammar());
-
-                recognizer.SpeechRecognized += 
-                    new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
-
-                recognizer.SetInputToDefaultAudioDevice();
-                recognizer.RecognizeAsync(RecognizeMode.Multiple);
-                while (true)
-                {
-                    Console.ReadLine();
-                }
-            }
+            Form1 frm1 = new Form1();
+            frm1.SpeechBox.Text += message;
         }
-        static void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+
+        private void Form1_Load(object sender, EventArgs e)
         {
-            Console.WriteLine("Recognized text: " + e.Result.Text);
+            recognizer.SetInputToDefaultAudioDevice();
+            recognizer.LoadGrammarAsync(new DictationGrammar());
+            recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+            recognizer.SpeechDetected += new EventHandler<SpeechDetectedEventArgs>(recognizer_StartListening);
+            recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+            startListening.SetInputToDefaultAudioDevice();
+            startListening.LoadGrammarAsync(new DictationGrammar());
+            startListening.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(startListening_SpeechRecognised);
+        }
+
+        public void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            SpeechBox.Text += e.Result.Text + "\n";
+        }
+
+        static void recognizer_StartListening(object sender, SpeechDetectedEventArgs e)
+        {
+        }
+
+        private void startListening_SpeechRecognised(object sender, SpeechRecognizedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Copy_Btn_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(SpeechBox.Text);
         }
     }
 }

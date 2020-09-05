5cc846d2105cb5a76b984e84f0a665e21c8bf46a
tager1199 <32684792+tager1199@users.noreply.github.com>
2020-09-05 13:05:24 +0100

    Base files
    Base files + creation of speech regonition device

---
+++ SpeecSynthesiser/SpeecSynthesiser/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+
+namespace SpeecSynthesiser
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine();
+        SpeechRecognitionEngine startListening = new SpeechRecognitionEngine();
+        Random rnd = new Random();
+        int RecTimeOut = 0;
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void runBtn_Click(object sender, EventArgs e)
+        {
+            using (SpeechRecognitionEngine recognizer =
+                new SpeechRecognitionEngine(
+                    new System.Globalization.CultureInfo("en-GB")))
+            {
+                recognizer.LoadGrammar(new DictationGrammar());
+
+                recognizer.SpeechRecognized += 
+                    new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+
+                recognizer.SetInputToDefaultAudioDevice();
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+                while (true)
+                {
+                    Console.ReadLine();
+                }
+            }
+        }
+        static void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            Console.WriteLine("Recognized text: " + e.Result.Text);
+        }
+    }
+}
